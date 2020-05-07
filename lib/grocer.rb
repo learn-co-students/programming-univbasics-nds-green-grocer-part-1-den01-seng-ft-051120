7827366413007041767766f5f47501efb3290762
Logan McGuire <lrmcguire93@gmail.com>
2020-05-07 23:42:20 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,28 @@
+
+require 'pry';
+
+#Find item in a collection and return if found, nil if not
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
-
+  collection.find { |item_hash| 
+    item_hash[:item] == name
+  }
 end
 
+#Enumerate through a cart of item hashes, adding a count and parsing down duplicates 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  cart = cart.reduce([]) { |item_collection, item_hash| 
+    #binding.pry; 
+    if find_item_by_name_in_collection(item_hash[:item], item_collection)
+      item_collection.find { |item_hash2| 
+        item_hash2[:item] == item_hash[:item]
+      }[:count] += 1; 
+      item_collection; 
+    else 
+      i = item_collection.length
+      item_collection[i] = item_hash;
+      item_collection[i][:count] = 1; 
+      item_collection; 
+    end 
+  }
 end
 
-
-  
